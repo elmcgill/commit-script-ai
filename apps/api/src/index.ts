2e9672a0f24c139819d7d03725c7a38339db247a
UnknownKoder <unknownkoderyt@gmail.com>
2026-03-19 18:15:59 -0500

    getting close to making our first llm call

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,11 +1,26 @@
+import axios from "axios";
 import express from "express";
 
 const app = express();
 
 app.use(express.json());
 
-app.post("/api/generate", (req, res) => {
-  res.json({ message: "API is working" });
+const OLLAMA_HOST = "http://localhost:11434";
+
+async function askOllama(prompt:string){
+    const res = await axios.post(`${OLLAMA_HOST}/api/generate`, {
+        model: "llama3",
+        prompt,
+        stream: false
+    })
+
+    return res.data.response;
+}
+
+app.post("/api/generate", async (req, res) => {
+    const prompt = req.body.prompt;
+    const ollamaResponse = await askOllama(prompt);
+    res.json({ message: ollamaResponse });
 });
 
 app.listen(3000, () => {
